6a7c46974e26d801b70d8f10a58975ff812f41ab
Antoni Król <a.krol@ufin.pl>
2024-06-13 11:54:20 +0200

    Innit

---
+++ todo-ui/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+      <VRow>
+        <VCard>
+          <VCardTitle>
+            Hello
+          </VCardTitle>
+        </VCard>
+      </VRow>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
